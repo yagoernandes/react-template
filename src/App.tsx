abd166c12508213ea923669e1fc616d0447c53d3
Yago <yago700@gmail.com>
2020-01-26 01:23:46 -0300

    alterações

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Provider } from 'react-redux'
+import store from './store'
+
+import { Page, Title, SubTitle } from './styles'
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Provider store={store}>
+			<Page>
+				<Title>Olá mundo!</Title>
+				<SubTitle>GERADO COM O TEMPLATE DO YAGO</SubTitle>
+			</Page>
+		</Provider>
 	)
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+require('dotenv').config()
+
+const aplicationEntryPoint = React.createElement(App)
+
+ReactDOM.render(aplicationEntryPoint, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
