09204bd7fb4433588ae225febbcab624fcb9ffea
YagoErnandes <yago700@gmail.com>
2020-05-26 17:42:45 -0300

    config

---
+++ src/config/colors.ts
@@ -0,0 +1,7 @@
+export const {
+  BACKGROUND_COLOR = '#eee',
+  PRIMARY_COLOR = '',
+  SECUNDARY_COLOR = '',
+  ERROR_COLOR = '',
+  SUCCESS_COLOR = '',
+} = process.env
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
+import { BACKGROUND_COLOR } from '../config/colors'
 
 export const Page = styled.div`
 	display: flex;
 	height: 100%;
 	width: 100%;
 	flex-direction: column;
-	background-color: #eee;
+	background-color: ${BACKGROUND_COLOR};
 	justify-content: center;
 	align-items: center;
 
