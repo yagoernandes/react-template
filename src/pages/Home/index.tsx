e6ee6bcd2dbefa080f4d962f72fcccbaf283da40
YagoErnandes <yago700@gmail.com>
2020-05-27 23:17:58 -0300

    Links nas paginas de exemplo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Page } from '../../styles'
+import { Link } from 'react-router-dom'
 
 const Home: React.FC = () => {
 	return (
 		<Page>
 			<h1>Olá mundo!</h1>
 			<h2>GERADO COM O TEMPLATE DO YAGO</h2>
+			<br/>
+			<Link to="/second">Segunda página</Link>
 		</Page>
 	)
 }
--- src/pages/SecondPage/index.tsx
+++ src/pages/SecondPage/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Page } from '../../styles'
 
+import { Link } from 'react-router-dom'
+
 const Home: React.FC = () => {
 	return (
 		<Page>
 			<h1>Segunda página!</h1>
 			<h2>GERADO COM O TEMPLATE DO YAGO</h2>
+			<br />
+			<Link to="/">Voltar para home</Link>
 		</Page>
 	)
 }
